b6ef9bbd0d0888e47a544da7e2ee306aa0e87e36
bagoesrex <bagoesrex@gmail.com>
2025-04-25 06:54:15 +0700

    chore: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BelanjaKu',
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 144, 238, 144),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(255, 34, 45, 38),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 40, 50, 42),
+      ),
+      home: Placeholder(),
+    );
+  }
+}
