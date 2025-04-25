85947e79285e62fd43e62f22026499810050c2fd
bagoesrex <bagoesrex@gmail.com>
2025-04-25 18:07:34 +0700

    feat: add newitem screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:sakubelanja_app/data/dummy_items.dart';
+import 'package:sakubelanja_app/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Daftar Belanjaan')),
+      appBar: AppBar(
+        title: const Text('Daftar Belanjaan'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
+      ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder:
