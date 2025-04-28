119c222d46fb94698e9a88188b64da2f6a113f7f
bagoesrex <bagoesrex@gmail.com>
2025-04-28 19:58:04 +0700

    feat: implement swipe to delete grocery item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -26,6 +26,12 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(child: Text('Tidak ada item.'));
@@ -34,14 +40,20 @@ class _GroceryListState extends State<GroceryList> {
       content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder:
-            (ctx, index) => ListTile(
-              title: Text(_groceryItems[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: _groceryItems[index].category.color,
+            (ctx, index) => Dismissible(
+              onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+              },
+              key: ValueKey(_groceryItems[index].id),
+              child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text(_groceryItems[index].quantity.toString()),
               ),
-              trailing: Text(_groceryItems[index].quantity.toString()),
             ),
       );
     }
